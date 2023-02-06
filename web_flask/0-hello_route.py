75459b9558ab7441648fbc2f222f5308c3330cdf
Amanuel2000 <amanuelf44@gmail.com>
2023-02-06 19:09:52 +0000

    web_flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+ elistening on 0.0.0.0, port 5000
+rt
+"""
+from flask import Flask
+
+app = Flask("__name__")
+
+@app.route('/', strict_slashes=False)
+
+
+def hello():
+    """Return a given string"""
+
+    return ("Hello HBNB!")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
